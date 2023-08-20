e2666fd58e20ea3e7ee7b12a89fe39c91a250fa2
Rizul Thakur <rizul.thakur1@gmail.com>
2023-08-20 11:55:09 +0530

    fixed dotenv

--- backend/authMiddleware.js
+++ backend/authMiddleware.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken");
+require('dotenv').config();
 
 const secret = process.env.SECRET;
 const secretKey = secret; // Secret key for signing and verifying tokens
@@ -8,13 +9,19 @@ const authMiddleware = (req, res, next) => {
 
   if (!token) {
     return res.status(401).json({ errorMessage: "Access denied", status: false });
+    console.log(token);
   }
 
   try {
     const decoded = jwt.verify(token, secretKey);
+    console.log("here1");
+    console.log(decoded);
     req.user = decoded.userId; // Store the user ID in the request object
     next(); // Move to the next middleware or route handler
   } catch (err) {
+    console.log(secret);
+    console.log(secretKey);
+    console.log(token)
     res.status(400).json({ errorMessage: "Invalid token", status: false });
   }
 };
